617f59387508480c4d8dadbf14c18d3b0d9e3a96
excellent <flarpzz08@gmail.com>
2022-02-02 22:33:50 +0200

    The first commit of SelfWriter (v1.0)

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace SelfWriter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int delay = 1;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            
+
+            var curDir = Directory.GetCurrentDirectory();
+            var curFilePath = Directory.GetParent(curDir)?.Parent?.Parent?.GetFiles("*.cs")[0].FullName;
+
+            var allCodeLines = File.ReadAllLines(curFilePath);
+
+            foreach (var line in allCodeLines)
+            {
+                foreach (var word in line.Split())
+                {
+                    Console.Write(word + " ");
+                    Thread.Sleep(delay);
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+    }
+}
